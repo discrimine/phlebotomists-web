87c1b063f205c48e8cadd1b5da6c197f1bbb2349
discrimine <discrimine96@gmail.com>
2020-02-04 01:11:30 +0200

    added google maps api

--- phlebotomists/src/app/users/components/dashboard/components/create-order-dialog/create-order-dialog.component.ts
+++ phlebotomists/src/app/users/components/dashboard/components/create-order-dialog/create-order-dialog.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Subscription, BehaviorSubject } from 'rxjs';
+
+import { MatSelectList } from 'src/app/core/interfaces/angular-material.interfaces';
+import { UsersService } from 'src/app/users/services/users.service';
+import { map } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-order-dialog',
   templateUrl: './create-order-dialog.component.html',
   styleUrls: ['./create-order-dialog.component.scss']
 })
-export class CreateOrderDialogComponent implements OnInit {
+export class CreateOrderDialogComponent implements OnInit, OnDestroy {
 
   public order: FormGroup;
+  public equipmentList: BehaviorSubject<MatSelectList<String>[]>;
+
+  private subscriptions: Subscription[];
 
   constructor(
     public dialogRef: MatDialogRef<CreateOrderDialogComponent>,
     // TODO: provide type if needed
     @Inject(MAT_DIALOG_DATA) public data: any,
     private formBuilder: FormBuilder,
+    private usersService: UsersService,
   ) {
     this.order = this.formBuilder.group({
       doctor: ['', Validators.required],
@@ -25,6 +35,9 @@ export class CreateOrderDialogComponent implements OnInit {
       location: ['', Validators.required],
       equipment: [[]],
     });
+    this.subscriptions = [];
+    this.equipmentList = new BehaviorSubject([]);
+    this.initEquipmentList();
   }
 
   ngOnInit() {
@@ -33,6 +46,25 @@ export class CreateOrderDialogComponent implements OnInit {
 
   public submitOrder(): void {
     console.log(this.order.getRawValue());
+  }
+
+  private catchErr(error): void {
+    console.log(error);
+  }
+
+  private initEquipmentList(): void {
+    this.subscriptions.push(this.usersService.getEquipmentList()
+      .pipe(
+        map((equipmentList: MatSelectList<string>[]) => {
+          return Array.isArray(equipmentList) ? equipmentList : [];
+        })
+      )
+      .subscribe(
+        (equipmentList: MatSelectList<string>[]): void => {
+          this.equipmentList.next(equipmentList);
+        },
+        (error: HttpErrorResponse) => this.catchErr(error),
+      ));
   }
 
 }
